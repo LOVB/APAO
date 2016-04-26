b06188b5d3dc93f9a9d2248be6476af5381cc23e
LoVB <lovbstudio@gmail.com>
2016-04-26 21:56:27 +0200

    Primer commit
    Archivos iniciales. Solo header

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>APAO</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fonts/iconmoon.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+</head>
+<body>
+<div class="container-fluid">
+    <header>
+        <div class="row">
+            <div id="cabecera" class="col-xs-12 hidden-xs">
+                <div id="logo">
+                    <img src="img/logo%20header.png" class="img-responsive" alt="logo">
+                </div>
+                <div id="descripcion">
+                    <span>23 de mayo de 2016</span>
+                    <h2>Asociación de Profesionales Andaluces de Orientación Profesional</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row navpills hidden-xs">
+            <ul id="menu" class="nav nav-pills">
+                <li role="presentation"><a href="#">INICIO</a></li>
+                <li role="presentation"><a href="#">NOTICIAS</a></li>
+                <li role="presentation"><a href="#">OPINIONES</a></li>
+                <li role="presentation"><a href="#">RECURSOS</a></li>
+                <li role="presentation"><a href="#">SOCIOS</a></li>
+                <li role="presentation"><a href="#">CONTACTO</a></li>
+            </ul>
+        </div>
+
+        <!-- Para medidas menores de 996px -->
+        <nav class="navbar navbar-default visible-xs">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="img/logo%20header%20blanco.png" alt="Logo APAO"></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li role="presentation"><a href="#">INICIO</a></li>
+                        <li role="presentation"><a href="#">NOTICIAS</a></li>
+                        <li role="presentation"><a href="#">OPINIONES</a></li>
+                        <li role="presentation"><a href="#">RECURSOS</a></li>
+                        <li role="presentation"><a href="#">SOCIOS</a></li>
+                        <li role="presentation"><a href="#">CONTACTO</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    </header>
+
+</div><!-- Final container fluid -->
+
+<!-- SCRIPTS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
